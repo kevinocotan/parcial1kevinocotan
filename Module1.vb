958f41df858a27c4646f15b8acffbf6482b9c99f
kevinocotan <kaocotan@gmail.com>
2021-03-12 19:29:26 -0600

    definicion de variables

---
+++ Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Sub Main()
+        Dim valores(14) As Integer 'agregar en numeros 14'
+        Dim media As Double = 0
+        Dim suma As Integer = 0
+        Dim opcion As Integer
+        Dim res As Integer = 1
+        Dim num(0 To 14) As Integer
+        Dim auxiliar(0 To 14) As Integer
+        Dim posicion As Integer = 0
+        Dim numero As Integer = 0
+        Dim contador2 As Integer = 0
+        Dim mayor As Integer = 0
+        Dim posicionmayor As Integer = 0
+
+
+
+
+    End Sub
+
+End Module
